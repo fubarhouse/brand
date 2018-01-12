7dfb758ec05b9fa1452a14f9c0bf18b316981bbc
Karl Hepworth <karl.hepworth@gmail.com>
2018-01-12 23:23:48 +1100

    Get the data structures working

--- src/Brand/Brand.php
+++ src/Brand/Brand.php
@@ -43,32 +43,36 @@ class Brand implements BrandInterface {
         $data->orderBy('n.date_created', 'ASC');
 
         // Execute and store it!
-        $this::$Raw = $data->execute()->fetchAll();
+        $dataset = $data->execute()->fetchAll();
+        $this::$Raw = end($dataset);
+        unset($dataset);
+        var_dump($this::$Raw);
 
         // Return FALSE if nothing came back.
         if (empty($this::$Raw)) {
+            $this::$Brand = NULL;
             return FALSE;
         }
 
         // Otherwise turn it into a structured array.
         else {
             $obj = (object) array(
-                'title' => $this::$Raw['title'],
-                'machine_name' => $this::$Raw['machine_name'],
-                'description' => $this::$Raw['description'],
-                'assets_inherit' => $this::$Raw['assets_inherit'],
-                'date_created' => $this::$Raw['date_created'],
-                'date_lock' => $this::$Raw['date_lock'],
-                'date_start' => $this::$Raw['date_start'],
-                'date_finish' => $this::$Raw['date_finish'],
-                'path_assets' => $this::$Raw['path_assets'],
-                'path_search' => $this::$Raw['path_search'],
-                'path_visibility' => $this::$Raw['path_visibility'],
-                'bid' => $this::$Raw['bid'],
-                'rid' => $this::$Raw['rid'],
-                'tid' => $this::$Raw['tid'],
-                'uid' => $this::$Raw['uid'],
-                'vid' => $this::$Raw['vid'],
+                'title' => $this::$Raw->title,
+                'machine_name' => $this::$Raw->machine_name,
+                'description' => $this::$Raw->description,
+                'assets_inherit' => $this::$Raw->assets_inherit,
+                'date_created' => $this::$Raw->date_created,
+                'date_lock' => $this::$Raw->date_lock,
+                'date_start' => $this::$Raw->date_start,
+                'date_finish' => $this::$Raw->date_finish,
+                'path_assets' => $this::$Raw->path_assets,
+                'path_search' => $this::$Raw->path_search,
+                'path_visibility' => $this::$Raw->path_visibility,
+                'bid' => $this::$Raw->bid,
+                'rid' => $this::$Raw->rid,
+                'tid' => $this::$Raw->tid,
+                'uid' => $this::$Raw->uid,
+                'vid' => $this::$Raw->vid,
             );
 
             $this::$Brand = $obj;
@@ -117,7 +121,7 @@ class Brand implements BrandInterface {
         $tid = (isset($options['assets_inherit'])) ? $options['tid'] : 0;
         $uid = (isset($options['assets_inherit'])) ? $options['uid'] : 0;
         $vid = (isset($options['assets_inherit'])) ? $options['vid'] : 0;
-        $mapped_fields = array(
+        $mapped_fields = (object) array(
             'title' => $title,
             'machine_name' => $machine_name,
             'description' => $description,
@@ -144,8 +148,8 @@ class Brand implements BrandInterface {
      * @inheritdoc
      */
     public function remove() {
-        $machine_name = $this::$Brand['machine_name'];
-        $timestamp =  $this::$Brand['date_created'];
+        $machine_name = $this::$Brand->machine_name;
+        $timestamp =  $this::$Brand->date_created;
         if ($timestamp > 0) {
             db_delete('brand')
                 ->condition('machine_name', $machine_name, '=')
