fa4c9b6ceb4973506d874c3cf83fc59d93de4e66
Karl Hepworth <karl.hepworth@gmail.com>
2018-01-12 21:16:56 +1100

    Fill in the Brand class implementing the BrandInterface

---
+++ src/Brand/Brand.php
@@ -0,0 +1,34 @@
+<?php
+
+class Brand implements BrandInterface {
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct() {}
+
+    /**
+     * @inheritdoc
+     */
+    public function check() {}
+
+    /**
+     * @inheritdoc
+     */
+    public function find() {}
+
+    /**
+     * @inheritdoc
+     */
+    public function get() {}
+
+    /**
+     * @inheritdoc
+     */
+    public function add() {}
+
+    /**
+     * @inheritdoc
+     */
+    public function remove() {}
+}
--- src/BrandInterface/BrandInterface.php
+++ src/BrandInterface/BrandInterface.php
@@ -1,1 +1,25 @@
 <?php
+
+/**
+ * @file
+ * Interfaces for the Brand class.
+ */
+
+/**
+ * The interface for the Brand class describes the available API.
+ */
+interface BrandInterface {
+
+    public function __construct();
+
+    public function check() ;
+
+    public function find() ;
+
+    public function get();
+
+    public function add();
+
+    public function remove();
+
+}
