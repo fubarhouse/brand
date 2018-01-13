b1b3e61f5f3a0a453a5234884d75e815aadee908
Karl Hepworth <karl.hepworth@gmail.com>
2018-01-13 21:30:24 +1100

    Adds a until function (w/ accompanying class) with some primitive checks.

--- src/Brands/Brands.php
+++ src/Brands/Brands.php
@@ -1,7 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: karl
- * Date: 12/1/18
- * Time: 8:12 PM
- */
+
+class Brands {
+
+    public static $Brands;
+
+    public function __construct(string $machine_name = NULL, bool $latest_only = FALSE) {
+        $data = db_select('brand', 'n')
+            ->fields('n', [
+                'title',
+                'machine_name',
+                'description',
+                'assets_inherit',
+                'date_created',
+                'date_lock',
+                'date_start',
+                'date_finish',
+                'path_assets',
+                'path_search',
+                'path_visibility',
+                'bid',
+                'rid',
+                'tid',
+                'uid',
+                'vid',
+            ]);
+
+        // If a machine name was specified, add it as a condition.
+        if (NULL !== $machine_name) {
+            $data->condition('machine_name', $machine_name, '=');
+        }
+
+        if ($latest_only !== TRUE) {
+            $data->distinct('n.machine_name');
+        }
+
+        // Sort it.
+        $data->orderBy('n.date_created', 'ASC');
+
+        // Execute and store it!
+        $this::$Brands = $data->execute()->fetchAll();
+    }
+}
