f8805b8de316c2cb703452c9befa9e0bc2470aab
Karl Hepworth <karl.hepworth@gmail.com>
2018-01-23 22:00:08 +1100

    Fix up self/this declarations

--- src/Brand/Brand.php
+++ src/Brand/Brand.php
@@ -73,54 +73,114 @@ class Brand implements BrandInterface {
 
       // Execute and store it!
       $dataset = $data->execute()->fetchAll();
-      $this::$Raw = end($dataset);
+      self::$Raw = end($dataset);
       unset($dataset);
-      var_dump($this::$Raw);
+      var_dump(self::$Raw);
 
       // Return FALSE if nothing came back.
-      if (empty($this::$Raw)) {
-        $this::$Brand = NULL;
+      if (empty(self::$Raw)) {
+        self::$Brand = NULL;
         return FALSE;
       }
 
       // Otherwise turn it into a structured array.
       else {
         $obj = (object) array(
-          'title' => $this::$Raw->title,
-          'machine_name' => $this::$Raw->machine_name,
-          'description' => $this::$Raw->description,
-          'assets_inherit' => $this::$Raw->assets_inherit,
-          'date_created' => $this::$Raw->date_created,
-          'date_lock' => $this::$Raw->date_lock,
-          'date_start' => $this::$Raw->date_start,
-          'date_finish' => $this::$Raw->date_finish,
-          'path_assets' => $this::$Raw->path_assets,
-          'path_search' => $this::$Raw->path_search,
-          'path_visibility' => $this::$Raw->path_visibility,
-          'meta_chrome' => $this::$Raw->meta_chrome,
-          'meta_viewport' => $this::$Raw->meta_viewport,
-          'http_robots' => $this::$Raw->http_robots,
-          'bid' => $this::$Raw->bid,
-          'rid' => $this::$Raw->rid,
-          'tid' => $this::$Raw->tid,
-          'uid' => $this::$Raw->uid,
-          'vid' => $this::$Raw->vid,
+          'title' => self::$Raw->title,
+          'machine_name' => self::$Raw->machine_name,
+          'description' => self::$Raw->description,
+          'assets_inherit' => self::$Raw->assets_inherit,
+          'date_created' => self::$Raw->date_created,
+          'date_lock' => self::$Raw->date_lock,
+          'date_start' => self::$Raw->date_start,
+          'date_finish' => self::$Raw->date_finish,
+          'path_assets' => self::$Raw->path_assets,
+          'path_search' => self::$Raw->path_search,
+          'path_visibility' => self::$Raw->path_visibility,
+          'meta_chrome' => self::$Raw->meta_chrome,
+          'meta_viewport' => self::$Raw->meta_viewport,
+          'http_robots' => self::$Raw->http_robots,
+          'bid' => self::$Raw->bid,
+          'rid' => self::$Raw->rid,
+          'tid' => self::$Raw->tid,
+          'uid' => self::$Raw->uid,
+          'vid' => self::$Raw->vid,
         );
       }
 
     }
-    $this::$Brand = $obj;
+    self::$Brand = $obj;
   }
 
     /**
      * @inheritdoc
      */
-    public function check() {
-        if (isset($this::$Brand->machine_name) && $this::$Brand->machine_name !== NULL) {
-            return TRUE;
-        } else {
-            return FALSE;
+    public function check() : bool {
+      if ($node = menu_get_object()) {
+
+        // Book.
+        if (module_exists('book')) {
+          if (isset($node->book['bid'])) {
+            if ((int) $node->book['bid'] === (int) self::$Brand->bid) {
+              return TRUE;
+            }
+          }
         }
+
+        // Nodes.
+        if (module_exists('node')) {
+          $delimiter = preg_split('/\s+/', self::$Brand->path_visibility);
+          foreach ($delimiter as $node_target) {
+
+            // Look for "node/xxx" in the path_visibility field:
+            if ($node = menu_get_object()) {
+              if (strpos($node_target, 'node/' . $node->nid) !== FALSE) {
+                return TRUE;
+              }
+            }
+
+            // Check if the current node alias is the node target.
+            if ($node_target === current_path()) {
+              return TRUE;
+            }
+
+            // Check if the current node path matches the node target.
+            if (drupal_match_path(current_path(), $node_target)) {
+              return TRUE;
+            }
+
+            // Check if the current node alias matches the node target.
+            if (drupal_match_path(drupal_get_path_alias(), $node_target)) {
+              return TRUE;
+            }
+          }
+        }
+
+        // Taxonomy.
+        if (module_exists('taxonomy')) {
+          foreach ((array) field_info_instances('node', $node->type) as $field_name => $info) {
+            foreach ((array) field_get_items('node', $node, $field_name) as $item) {
+              if (is_array($item) && !empty($item['tid']) && self::$Brand->tid == $item['tid']) {
+                return TRUE;
+              }
+            }
+          }
+        }
+      }
+
+      // Paths.
+      $delimiter = preg_split('/\s+/', self::$Brand->path_visibility);
+      foreach ($delimiter as $path_target) {
+        if (!empty(trim($path_target))) {
+          if (isset($_GET['destination']) && drupal_match_path($_GET['destination'], $path_target)) {
+            return TRUE;
+          }
+          elseif (isset($_GET['q']) && drupal_match_path($_GET['q'], $path_target)) {
+            return TRUE;
+          }
+        }
+      }
+      return FALSE;
     }
 
     /**
@@ -164,15 +224,15 @@ class Brand implements BrandInterface {
         );
         $query->fields($mapped_fields);
         $query->execute();
-        $this::$Brand = (object) $mapped_fields;
+      self::$Brand = (object) $mapped_fields;
     }
 
     /**
      * @inheritdoc
      */
     public function remove() {
-        $machine_name = $this::$Brand->machine_name;
-        $timestamp = $this::$Brand->date_created;
+        $machine_name = self::$Brand->machine_name;
+        $timestamp = self::$Brand->date_created;
         if ($timestamp > 0) {
             db_delete('brand')
                 ->condition('machine_name', $machine_name, '=')
