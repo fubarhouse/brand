48edabc42a46a568063f111dec81911288d325cb
Karl Hepworth <karl.hepworth@gmail.com>
2018-02-06 21:03:43 +1100

    Coding standards compliance.
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- src/Brand/Brand.php
+++ src/Brand/Brand.php
@@ -102,80 +102,80 @@ class Brand implements BrandInterface {
     self::$Brand = $obj;
   }
 
-    /**
-     * Check the brand to see if it's supposed to show.
-     *
-     * @inheritdoc
-     */
-    public function Check() : bool {
-      if (user_has_role(self::$Brand->rid) || ((int) user_role_load(self::$Brand->rid)->rid === DRUPAL_ANONYMOUS_RID)) {
-        if ((time() >= (int) self::$Brand->date_start && time() <= self::$Brand->date_finish) || (int) self::$Brand->date_lock === 1) {
-          return TRUE;
-        }
+  /**
+   * Check the brand to see if it's supposed to show.
+   *
+   * @inheritdoc
+   */
+  public function Check() : bool {
+    if (user_has_role(self::$Brand->rid) || ((int) user_role_load(self::$Brand->rid)->rid === DRUPAL_ANONYMOUS_RID)) {
+      if ((time() >= (int) self::$Brand->date_start && time() <= self::$Brand->date_finish) || (int) self::$Brand->date_lock === 1) {
+        return TRUE;
       }
-      return FALSE;
     }
+    return FALSE;
+  }
 
-    /**
-     * Create a new database row based upon the current object.
-     *
-     * @inheritdoc
-     */
-    public function add(string $machine_name, array $options = array()) {
-        $query = db_insert('brand');
-        $now = new DateTime();
-        $title = (isset($options['title'])) ? $options['title'] : '';
-        $description = (isset($options['description'])) ? $options['description'] : '';
-        $date_created = $now->getTimestamp();
-        $date_lock = (isset($options['date_lock'])) ? $options['date_lock'] : 0;
-        $date_start = (isset($options['date_start'])) ? $options['date_start'] : $now->getTimestamp();
-        $date_finish = (isset($options['date_finish'])) ? $options['date_finish'] : $now->getTimestamp();
-        $path_visibility = (isset($options['path_visibility'])) ? $options['path_visibility'] : '';
-        $theme = (isset($options['theme'])) ? $options['theme'] : '';
-        $bid = (isset($options['bid'])) ? $options['bid'] : 0;
-        $rid = (isset($options['rid'])) ? $options['rid'] : 0;
-        $tid = (isset($options['tid'])) ? $options['tid'] : 0;
-        $uid = (isset($options['uid'])) ? $options['uid'] : 0;
-        $vid = (isset($options['vid'])) ? $options['vid'] : 0;
-        $mapped_fields = array(
-            'title' => $title,
-            'machine_name' => $machine_name,
-            'description' => $description,
-            'date_created' => $date_created,
-            'date_lock' => $date_lock,
-            'date_start' => $date_start,
-            'date_finish' => $date_finish,
-            'path_visibility' => $path_visibility,
-            'theme' => $theme,
-            'bid' => $bid,
-            'rid' => $rid,
-            'tid' => $tid,
-            'uid' => $uid,
-            'vid' => $vid,
-        );
-        $query->fields($mapped_fields);
-        $query->execute();
-      self::$Brand = (object) $mapped_fields;
+  /**
+   * Create a new database row based upon the current object.
+   *
+   * @inheritdoc
+   */
+  public function add(string $machine_name, array $options = array()) {
+    $query = db_insert('brand');
+    $now = new DateTime();
+    $title = (isset($options['title'])) ? $options['title'] : '';
+    $description = (isset($options['description'])) ? $options['description'] : '';
+    $date_created = $now->getTimestamp();
+    $date_lock = (isset($options['date_lock'])) ? $options['date_lock'] : 0;
+    $date_start = (isset($options['date_start'])) ? $options['date_start'] : $now->getTimestamp();
+    $date_finish = (isset($options['date_finish'])) ? $options['date_finish'] : $now->getTimestamp();
+    $path_visibility = (isset($options['path_visibility'])) ? $options['path_visibility'] : '';
+    $theme = (isset($options['theme'])) ? $options['theme'] : '';
+    $bid = (isset($options['bid'])) ? $options['bid'] : 0;
+    $rid = (isset($options['rid'])) ? $options['rid'] : 0;
+    $tid = (isset($options['tid'])) ? $options['tid'] : 0;
+    $uid = (isset($options['uid'])) ? $options['uid'] : 0;
+    $vid = (isset($options['vid'])) ? $options['vid'] : 0;
+    $mapped_fields = array(
+      'title' => $title,
+      'machine_name' => $machine_name,
+      'description' => $description,
+      'date_created' => $date_created,
+      'date_lock' => $date_lock,
+      'date_start' => $date_start,
+      'date_finish' => $date_finish,
+      'path_visibility' => $path_visibility,
+      'theme' => $theme,
+      'bid' => $bid,
+      'rid' => $rid,
+      'tid' => $tid,
+      'uid' => $uid,
+      'vid' => $vid,
+    );
+    $query->fields($mapped_fields);
+    $query->execute();
+    self::$Brand = (object) $mapped_fields;
+  }
+
+  /**
+   * Remove a database row based upon the current object.
+   *
+   * @inheritdoc
+   */
+  public function remove(int $timestamp = NULL) {
+    $machine_name = self::$Brand->machine_name;
+
+    $q = db_delete('brand');
+    $q->condition('machine_name', $machine_name, '=');
+    if ($timestamp !== NULL) {
+      $q->condition('date_created', $timestamp, '=');
+      $q->execute();
+      drupal_set_message("The record timestamped {$timestamp} for brand {$machine_name} has been removed.");
     }
-
-    /**
-     * Remove a database row based upon the current object.
-     *
-     * @inheritdoc
-     */
-    public function remove(int $timestamp = NULL) {
-        $machine_name = self::$Brand->machine_name;
-
-        $q = db_delete('brand');
-        $q->condition('machine_name', $machine_name, '=');
-        if ($timestamp !== NULL) {
-          $q->condition('date_created', $timestamp, '=');
-          $q->execute();
-          drupal_set_message("The record timestamped {$timestamp} for brand {$machine_name} has been removed.");
-        }
-        else {
-          $q->execute();
-          drupal_set_message("The brand {$machine_name} has been removed.");
-        }
+    else {
+      $q->execute();
+      drupal_set_message("The brand {$machine_name} has been removed.");
     }
+  }
 }
--- src/Brands/Brands.php
+++ src/Brands/Brands.php
@@ -7,51 +7,51 @@
 
 class Brands implements BrandsInterface {
 
-    public static $Brands;
+  public static $Brands;
 
-    public function __construct(string $machine_name = NULL, bool $latest_only = FALSE) {
-        $data = db_select('brand', 'n')
-            ->fields('n', [
-                'title',
-                'machine_name',
-                'description',
-                'date_created',
-                'date_lock',
-                'date_start',
-                'date_finish',
-                'path_visibility',
-                'bid',
-                'rid',
-                'tid',
-                'uid',
-                'vid',
-            ]);
+  public function __construct(string $machine_name = NULL, bool $latest_only = FALSE) {
+    $data = db_select('brand', 'n')
+      ->fields('n', [
+        'title',
+        'machine_name',
+        'description',
+        'date_created',
+        'date_lock',
+        'date_start',
+        'date_finish',
+        'path_visibility',
+        'bid',
+        'rid',
+        'tid',
+        'uid',
+        'vid',
+      ]);
 
-        // If a machine name was specified, add it as a condition.
-        if (NULL !== $machine_name) {
-            $data->condition('machine_name', $machine_name, '=');
-        }
-
-        if ($latest_only === TRUE) {
-            $data->distinct('n.machine_name');
-        }
-
-        // Sort it.
-        $data->orderBy('n.date_created', 'ASC');
-
-        // Execute and store it!
-      $AllData = $data->execute()->fetchAll();
-      $brands = array();
-      foreach ($AllData as $Data) {
-        // Additional filter for latest date
-        if ($latest_only === TRUE) {
-          $brands[$Data->machine_name] = new Brand($Data->machine_name, $Data->date_created);
-        }
-        else {
-          $brands[] = new Brand($Data->machine_name, $Data->date_created);
-        }
-      }
-
-      $this::$Brands = $brands;
+    // If a machine name was specified, add it as a condition.
+    if (NULL !== $machine_name) {
+      $data->condition('machine_name', $machine_name, '=');
     }
+
+    if ($latest_only === TRUE) {
+      $data->distinct('n.machine_name');
+    }
+
+    // Sort it.
+    $data->orderBy('n.date_created', 'ASC');
+
+    // Execute and store it!
+    $AllData = $data->execute()->fetchAll();
+    $brands = array();
+    foreach ($AllData as $Data) {
+      // Additional filter for latest date.
+      if ($latest_only === TRUE) {
+        $brands[$Data->machine_name] = new Brand($Data->machine_name, $Data->date_created);
+      }
+      else {
+      $brands[] = new Brand($Data->machine_name, $Data->date_created);
+      }
+    }
+
+    $this::$Brands = $brands;
+  }
 }
